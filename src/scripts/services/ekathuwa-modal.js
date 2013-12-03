4c27f32fb4d9d4902e6fa4c3e143bd851b08a4c2
Plastoria <cbaune@plastoria.be>
2013-12-03 16:37:37 +0100

    Use the "onload" angularjs instead of $timeout
    ngInclude has an "onload" attribute. That's better to use it than waiting 300ms and hoping dom is ready because if the network is a bit slow, the modal won't show !
    
    With this change, even with a slow network, the dialog would show.

--- src/scripts/services/ekathuwa-modal.js
+++ src/scripts/services/ekathuwa-modal.js
@@ -1,153 +1,152 @@
 /**
  * ekathuwa
- * @version v0.2.0 - 2013-11-24
- * @link https://github.com/sarath2
+ * @version v0.2.0 - 2013-11-26
+ * @link https://github.com/sarath2/ngEkathuwa
  * @author Sarath Ambegoda <sarath2mail@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
-'use strict';
-angular.module('ngEkathuwa', [])
-    .run(function ($rootScope, $ekathuwa) {
-        $rootScope.$ekathuwa = $ekathuwa;
-    })
-    .provider('$ekathuwa', function () {
-        this.$get = function ($compile, $rootScope, $timeout, $q) {
-            this.modal = function (op) {
-                var d = {
-                    id: "ekathuwaModalID",
-                    scope: $rootScope.$new(),
-                    controller: null,
-                    backdrop: true,
-                    keyboard: true,
-                    remote: false,
-                    show: true,
-                    modalClass: "fade",
-                    role: "dialog",
-                    contentStyle: null, //width:350px;heigth:400px;background-color:red;
-                    contentCustomSize: 0, //80%
-                    contentPreSize: "df", //df,sm,md,lg,fl
-                    templateURL: null,
-                    templateHTML: null,
-                    bodyTemplateURL: null,
-                    bodyTemplate: null,
-                    header: true,
-                    headerText: null,
-                    headerClass: "",
-                    headerTemplate: null,
-                    headerCloseBtn: true,
-                    footer: true,
-                    footerClass: "",
-                    footerTemplate: null,
-                    footerCloseBtn: true,
-                    footerCloseBtnText: 'Close',
-                    footerSaveBtn: false,
-                    footerSaveBtnText: 'Save changes'
-                };
-                var t = '',
-                    s = '',
-                    c = '',
-                    b = '',
-                    f = '',
-                    h = '';
-                op = angular.extend(d, op);
-                //set bootstrap's options
-                var btOPs = {
-                    backdrop: op.backdrop,
-                    keyboard: op.keyboard,
-                    remote: op.remote,
-                    show: op.show
-                };
-                //set current modal dom selector
-                var modSelector = "#" + op.id + " .modal";
-                //Set controller
-                c = op.controller ? 'data-ng-controller=' + op.controller : '';
-                //set modal by temp URL
-                if (op.templateURL !== null && op.templateURL !== '') {
-                    t = '<div ' + c + ' id="' + op.id + '" data-ng-include="\'' + op.templateURL + '\'"></div>';
-                } else {
-                    var a = '<div ' + c + ' id="' + op.id + '">';
-                    //set modal by temp HTML
-                    if (op.templateHTML !== null && op.templateHTML !== '') {
-                        t = a + op.templateHTML + '</div>';
-                    } else {
-                        //set modal body by temp URL
-                        if (op.bodyTemplateURL !== null && op.bodyTemplateURL !== '') {
-                            b = '<div class="modal-body" data-ng-include="\'' + op.bodyTemplateURL + '\'"></div>';
+(function (window, document, undefined) {
+    'use strict';
+    angular.module('ngEkathuwa', []);
+    angular.module('ngEkathuwa', []).run([
+            '$rootScope',
+            '$ekathuwa',
+            function ($rootScope, $ekathuwa) {
+                $rootScope.$ekathuwa = $ekathuwa;
+            }
+        ]).provider('$ekathuwa', function () {
+            this.$get = [
+                '$compile',
+                '$rootScope',
+                '$timeout',
+                '$q',
+                function ($compile, $rootScope, $timeout, $q) {
+                    this.modal = function (op) {
+                        var d = {
+                            id: 'ekathuwaModalID',
+                            scope: $rootScope.$new(),
+                            controller: null,
+                            backdrop: true,
+                            keyboard: true,
+                            remote: false,
+                            show: true,
+                            modalClass: 'fade',
+                            role: 'dialog',
+                            contentStyle: null,
+                            contentCustomSize: 0,
+                            contentPreSize: 'df',
+                            templateURL: null,
+                            templateHTML: null,
+                            bodyTemplateURL: null,
+                            bodyTemplate: null,
+                            header: true,
+                            headerText: null,
+                            headerClass: '',
+                            headerTemplate: null,
+                            headerCloseBtn: true,
+                            footer: true,
+                            footerClass: '',
+                            footerTemplate: null,
+                            footerCloseBtn: true,
+                            footerCloseBtnText: 'Close',
+                            footerSaveBtn: false,
+                            footerSaveBtnText: 'Save changes'
+                        };
+                        var t = '', s = '', c = '', b = '', f = '', h = '';
+                        op = angular.extend(d, op);
+                        var btOPs = {
+                            backdrop: op.backdrop,
+                            keyboard: op.keyboard,
+                            remote: op.remote,
+                            show: op.show
+                        };
+                        var modSelector = '#' + op.id + ' .modal';
+                        c = op.controller ? 'data-ng-controller=' + op.controller : '';
+                        if (op.templateURL !== null && op.templateURL !== '') {
+                            t = '<div ' + c + ' id="' + op.id + '" data-ng-include="\'' + op.templateURL + '\'" onload="ekathuaLoaded()"></div>';
                         } else {
-                            //set modal body by temp HTML
-                            if (op.bodyTemplate !== null && op.bodyTemplate !== '') {
-                                b = '<div class="modal-body">' + op.bodyTemplate + '</div>';
+                            var a = '<div ' + c + ' id="' + op.id + '">';
+                            if (op.templateHTML !== null && op.templateHTML !== '') {
+                                t = a + op.templateHTML + '</div>';
                             } else {
-                                b = '<div class="modal-body">Ekathuwa modal body.</div>';
-                            }
-                        }
-
-                        //set modal header HTML
-                        if (op.header) {
-                            if (op.headerTemplate !== null && op.headerTemplate !== '') {
-                                h = '<div class="modal-header ' + op.headerClass + '">' + op.headerTemplate + '</div>';
-                            } else {
-                                var ht = '';
-                                //set header text
-                                if (op.headerText !== null && op.headerText !== '') {
-                                    ht = '<h4 id="myModalLabel" class="modal-title">' + op.headerText + '</h4>';
+                                if (op.bodyTemplateURL !== null && op.bodyTemplateURL !== '') {
+                                    b = '<div class="modal-body" data-ng-include="\'' + op.bodyTemplateURL + '\'" onload="ekathuaLoaded()"></div>';
+                                } else {
+                                    if (op.bodyTemplate !== null && op.bodyTemplate !== '') {
+                                        b = '<div class="modal-body">' + op.bodyTemplate + '</div>';
+                                    } else {
+                                        b = '<div class="modal-body">Ekathuwa modal body.</div>';
+                                    }
                                 }
-                                //set modal default header
-                                h = '<div class="modal-header ' + op.headerClass + '"><button data-ng-if="' + op.headerCloseBtn + '" aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>' + ht + '</div>';
+                                if (op.header) {
+                                    if (op.headerTemplate !== null && op.headerTemplate !== '') {
+                                        h = '<div class="modal-header ' + op.headerClass + '">' + op.headerTemplate + '</div>';
+                                    } else {
+                                        var ht = '';
+                                        if (op.headerText !== null && op.headerText !== '') {
+                                            ht = '<h4 id="myModalLabel" class="modal-title">' + op.headerText + '</h4>';
+                                        }
+                                        h = '<div class="modal-header ' + op.headerClass + '"><button data-ng-if="' + op.headerCloseBtn + '" aria-hidden="true" data-dismiss="modal" class="close" type="button">\xd7</button>' + ht + '</div>';
+                                    }
+                                }
+                                if (op.footer) {
+                                    if (op.footerTemplate !== null && op.footerTemplate !== '') {
+                                        f = '<div class="modal-footer ' + op.footerClass + '">' + op.footerTemplate + '</div>';
+                                    } else {
+                                        f = '<div class="modal-footer ' + op.footerClass + '"><button data-ng-if="' + op.footerCloseBtn + '" data-dismiss="modal" class="btn btn-default" type="button">' + op.footerCloseBtnText + '</button><button data-ng-if="' + op.footerSaveBtn + '" class="btn btn-primary" type="button">' + op.footerSaveBtnText + '</button></div>';
+                                    }
+                                }
+                                t = a + '<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" class="modal fade" id="myModal" style="display: none;"><div class="modal-dialog"><div class="modal-content">' + h + b + f + '</div></div></div></div>';
                             }
                         }
-                        //set modal footer HTML
-                        if (op.footer) {
-                            if (op.footerTemplate !== null && op.footerTemplate !== '') {
-                                f = '<div class="modal-footer ' + op.footerClass + '">' + op.footerTemplate + '</div>';
-                            } else {
-                                //set modal default footer
-                                f = '<div class="modal-footer ' + op.footerClass + '"><button data-ng-if="' + op.footerCloseBtn + '" data-dismiss="modal" class="btn btn-default" type="button">' + op.footerCloseBtnText + '</button><button data-ng-if="' + op.footerSaveBtn + '" class="btn btn-primary" type="button">' + op.footerSaveBtnText + '</button></div>';
-                            }
+                        if (op.contentStyle !== null && op.contentStyle !== '') {
+                            s = op.contentStyle;
                         }
-                        t = a + '<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" class="modal fade" id="myModal" style="display: none;"><div class="modal-dialog"><div class="modal-content">' + h + b + f + '</div></div></div></div>';
-                    }
+                        if (op.contentCustomSize !== 0 && angular.isNumber(op.contentCustomSize) && op.contentCustomSize !== '' && op.contentCustomSize !== null) {
+                            s = s + ';width: ' + op.contentCustomSize + '%;';
+                        }
+                        switch (op.contentPreSize) {
+                            case 'sm':
+                                s = s + ';width:25%;';
+                                break;
+                            case 'md':
+                                s = s + ';width:50%;';
+                                break;
+                            case 'lg':
+                                s = s + ';width:75%;';
+                                break;
+                            case 'fl':
+                                s = s + ';width:100%;';
+                                break;
+                            default:
+                                break;
+                        }
+                        var mq = '#' + op.id + ' .modal-dialog { ' + s + '} @media (max-width: 768px) {' + '#' + op.id + ' .modal-dialog {width:90%;}}';
+                        angular.element('#ekathuwaSt' + op.id).remove();
+                        angular.element('head').append('<style id="ekathuwaSt' + op.id + '">' + mq + '</style>');
+                        angular.element('#' + op.id).remove();
+                        var m = angular.element(t);
+                        angular.element('body').append(m);
+
+                        var deferred = $q.defer();
+
+                        op.scope.ekathuaLoaded= function(){
+                            deferred.resolve(angular.element(modSelector).modal(btOPs));
+                        };
+
+                        $compile(m)(op.scope);
+
+                        if (op.templateURL !== null && op.templateURL !== '') {
+//                            $timeout(function () {
+//                                deferred.resolve(angular.element(modSelector).modal(btOPs));
+//                            }, 300);
+                        } else {
+                            deferred.resolve(angular.element(modSelector).modal(btOPs));
+                        }
+                        return deferred.promise;
+                    };
+                    return this;
                 }
-                //set style
-                if (op.contentStyle !== null && op.contentStyle !== '') {
-                    s = op.contentStyle;
-                }
-                if (op.contentCustomSize !== 0 && angular.isNumber(op.contentCustomSize) && op.contentCustomSize !== "" && op.contentCustomSize !== null) {
-                    s = s + ';width: ' + op.contentCustomSize + '%;';
-                }
-                switch (op.contentPreSize) {
-                case "sm":
-                    s = s + ';width:25%;';
-                    break;
-                case "md":
-                    s = s + ';width:50%;';
-                    break;
-                case "lg":
-                    s = s + ';width:75%;';
-                    break;
-                case "fl":
-                    s = s + ';width:100%;';
-                    break;
-                default:
-                    break;
-                }
-                var mq = "#" + op.id + ' .modal-dialog { ' + s + '} @media (max-width: 768px) {' + "#" + op.id + ' .modal-dialog {width:90%;}}';
-                angular.element("#ekathuwaSt" + op.id).remove();
-                angular.element('head').append('<style id="ekathuwaSt' + op.id + '">' + mq + '</style>');
-                angular.element("#" + op.id).remove();
-                var m = angular.element(t);
-                angular.element('body').append(m);
-                $compile(m)(op.scope);
-                var deferred = $q.defer();
-                if (op.templateURL !== null && op.templateURL !== '') {
-                    $timeout(function () {
-                        deferred.resolve(angular.element(modSelector).modal(btOPs));
-                    }, 300);
-                } else {
-                    deferred.resolve(angular.element(modSelector).modal(btOPs));
-                }
-                return deferred.promise;
-            };
-            return this;
-        };
-    });
+            ];
+        });
+})(window, document);
